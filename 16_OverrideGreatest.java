41e0400cc773b274e9cf3b4472525821e60322f4
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-06 07:41:31 +0530

    updated 16 th program for better understanding

--- 16_OverrideGreatest.java
+++ 16_OverrideGreatest.java
@@ -1,45 +1,32 @@
-class A {
-    int x;
-    int y;
-
-    A(int x, int y){
+// Find greatest of two or three numbers using method overriding
+class GreatestOfTwo {
+    int x, y;
+    GreatestOfTwo(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
-    int greatest(){
+    int greatest() {
         return (x > y) ? x : y;
     }
 }
 
-class B extends A {
+class GreatestOfThree extends GreatestOfTwo {
     int z;
-
-    B(int x, int y, int z){
+    GreatestOfThree(int x, int y, int z) {
         super(x, y);
         this.z = z;
     }
+    @Override
     int greatest() {
-        if (x > y && x > z) {
-            return x;
-        } else if (y > x && y > z) {
-            return y;
-        } else {
-            return z;
-        }
+        return Math.max(Math.max(x, y), z);
     }
 }
-public class thirtyeight {
 
+public class ThirtyEight {
     public static void main(String[] args) {
-        
-        A a=new A(5,10);
-        B b=new B(10, 5 ,25);
-        A z;
-        z=a;
-        System.err.println(z.greatest()+" is Greatest");
-        z=b;
-        System.out.println(z.greatest()+" is greater");
-
+        GreatestOfTwo two = new GreatestOfTwo(5, 10);
+        GreatestOfThree three = new GreatestOfThree(10, 5, 25);
+        System.out.println("Greatest of two: " + two.greatest());
+        System.out.println("Greatest of three: " + three.greatest());
     }
 }
