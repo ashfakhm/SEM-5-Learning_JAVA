51e3fb12378e3ce5e26b9e3c248b7fed2fdcb98f
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-06 07:54:10 +0530

    modified multithreading example to improve clarity and naming conventions

--- 18_ThreadHelloHai.java
+++ 18_ThreadHelloHai.java
@@ -1,24 +1,28 @@
-class Hai extends Thread {
+// Demonstrates multithreading in Java by printing "Hello" and "Hai" alternately
+// Each thread prints its message 6 times with a 500ms pause between prints
+
+class HaiThread extends Thread {
+    @Override
     public void run() {
         for (int i = 0; i <= 5; i++) {
             System.out.println("Hai");
             try {
-                Thread.sleep(500);
+                Thread.sleep(500); // Pause for 500 milliseconds
             } catch (InterruptedException e) {
-                System.out.println(e);
+                System.out.println("HaiThread interrupted: " + e);
             }
         }
     }
 }
 
-class Hello extends Thread {
+class HelloThread extends Thread {
     public void run() {
         for (int i = 0; i <= 5; i++) {
             System.out.println("Hello");
             try {
-                Thread.sleep(500);
+                Thread.sleep(500); // Pause for 500 milliseconds
             } catch (InterruptedException e) {
-                System.out.println(e);
+                System.out.println("HelloThread interrupted: " + e);
             }
         }
     }
@@ -26,10 +30,12 @@ class Hello extends Thread {
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        Hai h1 = new Hai();
-        Hello h2 = new Hello();
+        // Create thread objects
+        HaiThread haiThread = new HaiThread();
+        HelloThread helloThread = new HelloThread();
 
-        h1.start();
-        h2.start();
+        // Start both threads
+        haiThread.start();
+        helloThread.start();
     }
 }
