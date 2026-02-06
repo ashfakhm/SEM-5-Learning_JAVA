313ffc9fc9f8753a1a35095037e71549eaec774b
ASHFAKH M <ashfakhthedev@gmail.com>
2026-02-06 07:13:22 +0530

    simplified fiboncacci program

--- 04_Fibonacci.java
+++ 04_Fibonacci.java
@@ -1,25 +1,17 @@
 import java.util.*;
-class fibannoci{
-    public static void main(String args[])
-    {
-        Scanner s= new Scanner(System.in);
-        int start,end;
-        start = 0;
-        System.out.println("Enter The Limit of Fibonacci Series:");
-        end = s.nextInt();
-        int a=0,b=1;
-        System.out.println("Fibonnaci Numbers In The range " +start + " to " +end+ " : ");
-        while (a<=end) {
-            if(a>=start)
-            {
-               System.out.print(a + " "); 
-            }
+class Fibonacci {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the limit for Fibonacci Series:");
+        int limit = s.nextInt();
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Numbers up to " + limit + ": ");
+        while (a <= limit) {
+            System.out.print(a + " ");
             int next = a + b;
-            a=b;
-            b=next;
+            a = b;
+            b = next;
         }
         s.close();
-    
-
     }
 }
